92f2cb126205f42817c1e5b809e18d21796e9d3d
Jonny Hicks <hello.jonnyhicks@gmail.com>
2025-09-02 12:25:41 +0200

    down graded node to 18.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,16 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    chunkSizeWarningLimit: 1000,  // Increase warning threshold
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'maplibre': ['maplibre-gl', '@vis.gl/react-maplibre']
+        }
+      }
+    }
+  }
 })
